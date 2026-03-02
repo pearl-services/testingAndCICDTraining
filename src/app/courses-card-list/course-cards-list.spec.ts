07a24c4273603777c704e1cc371b0e4e89ed8d99
vasco <jhades.dev@gmail.com>
2026-03-02 17:04:06 +0100

    recording Angular Testing In Depth (Signals Edition)

--- src/app/courses-card-list/course-cards-list.spec.ts
+++ src/app/courses-card-list/course-cards-list.spec.ts
@@ -7,10 +7,13 @@ import {By} from '@angular/platform-browser';
 import {Dialog} from '@angular/cdk/dialog';
 import {CoursesCardList} from "./courses-card-list";
 import {CoursesDialog} from '../courses-dialog/courses-dialog';
+import {HelloWorld} from '../hello-world/hello-world';
+import {DebugElement} from '@angular/core';
 
 describe('CoursesCardList', () => {
   let component: CoursesCardList;
   let fixture: ComponentFixture<CoursesCardList>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,13 +23,17 @@ describe('CoursesCardList', () => {
 
     fixture = TestBed.createComponent(CoursesCardList);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
 
     fixture.componentRef.setInput('courses', MOCK_COURSES);
     fixture.detectChanges();
   });
 
   it('should display the course list', () => {
-
+    const cardTitles = de.queryAll(By.css(".course-card .card-header"));
+    expect(cardTitles.length).toBe(2);
+    const titleEl = cardTitles[0].nativeElement;
+    expect(titleEl.textContent).toBe("Beginner Course");
   });
 
 
