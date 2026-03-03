83cdcdec2d2b53d91670559a68afaf7614933035
vasco <jhades.dev@gmail.com>
2026-03-03 15:17:33 +0100

    recording Angular Testing In Depth (Signals Edition)

---
+++ src/app/courses/courses.spec.ts
@@ -0,0 +1,45 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {Course} from '../model/course';
+import {MOCK_COURSES} from '../testing/testing-data';
+import {describe, expect, it, vi, beforeEach, afterEach} from "vitest"
+import {HttpTestingController, provideHttpClientTesting} from '@angular/common/http/testing';
+import {ApplicationRef, DebugElement} from '@angular/core';
+import {Courses} from "./courses"
+import {CoursesService} from '../services/courses.service';
+import {provideHttpClient} from '@angular/common/http';
+import {provideRouter} from '@angular/router';
+import {By} from '@angular/platform-browser';
+
+describe('Courses', () => {
+  let component: Courses;
+  let fixture: ComponentFixture<Courses>;
+  let de: DebugElement;
+  let httpMock: HttpTestingController;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Courses],
+      providers: [
+        CoursesService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([])
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Courses);
+    component = fixture.componentInstance;
+    de = fixture.debugElement;
+    httpMock = TestBed.inject(HttpTestingController);
+    fixture.detectChanges();
+  });
+
+  it('should load courses and filter by category', async () => {
+
+  });
+
+  it('should show advanced course when tab clicked', async () => {
+
+  });
+
+});
