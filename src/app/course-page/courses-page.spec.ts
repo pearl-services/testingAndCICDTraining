1a54d229cdd077cc1f792a0bbd9d3dcf9f50387d
vasco <jhades.dev@gmail.com>
2026-03-10 16:03:09 +0100

    recording Angular Testing In Depth (Signals Edition)

--- src/app/course-page/courses-page.spec.ts
+++ src/app/course-page/courses-page.spec.ts
@@ -7,6 +7,7 @@ import {CoursesService} from '../services/courses.service';
 import {CoursePage} from './course-page';
 import {ActivatedRoute, Router} from '@angular/router';
 import {By} from '@angular/platform-browser';
+import {Lesson} from '../model/lesson';
 
 const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
@@ -18,7 +19,32 @@ describe('CoursePage', () => {
 
   beforeEach(async () => {
     mockCoursesService = {
-      findLessons: vi.fn().mockResolvedValue(MOCK_LESSONS)
+      findLessons: vi.fn(
+        async (
+          courseId: number,
+          filter = '',
+          sortOrder = 'asc',
+          pageNumber = 0,
+          pageSize = 3
+        ) => {
+
+          let lessons = MOCK_LESSONS.filter(l => l.courseId === courseId);
+
+          if (filter?.trim()) {
+            const q = filter.toLowerCase();
+            lessons = lessons.filter(l => l.description.toLowerCase().includes(q));
+          }
+
+          lessons = [...lessons].sort((a, b) => {
+            return sortOrder === 'asc' ? a.seqNo - b.seqNo : b.seqNo - a.seqNo;
+          });
+
+          const start = pageNumber * pageSize;
+          const end = start + pageSize;
+
+          return lessons.slice(start, end);
+        }
+      )
     };
 
     await TestBed.configureTestingModule({
@@ -37,13 +63,22 @@ describe('CoursePage', () => {
     component = fixture.componentInstance;
   });
 
+  function getRowDescription(de: DebugElement, index: number) {
+    const row = de.query(By.css(`tbody tr:nth-child(${index}) .description-cell`));
+    return row?.nativeElement?.textContent ?? null;
+  }
+
   it('should load lessons on init', async () => {
     await fixture.whenStable();
     fixture.detectChanges();
 
     expect(mockCoursesService.findLessons).toHaveBeenCalledWith(1, '', 'asc', 0, 3);
     const rows = de.queryAll(By.css('tbody tr'));
-    expect(rows.length).toBe(2);
+    expect(rows.length).toBe(3);
+
+    expect(getRowDescription(de, 1)).toBe("Lesson 1");
+    expect(getRowDescription(de, 2)).toBe("Lesson 2");
+    expect(getRowDescription(de, 3)).toBe("Lesson 3");
   });
 
   it('should show the loading spinner while fetching', async () => {
@@ -53,7 +88,7 @@ describe('CoursePage', () => {
     expect(component.loading()).toBe(true);
   });
 
-  it('should navigate to next or previous page', async () => {
+  it('should navigate to next page', async () => {
     await fixture.whenStable();
     fixture.detectChanges();
     expect(mockCoursesService.findLessons).toHaveBeenLastCalledWith(1, '', 'asc', 0, 3);
@@ -65,17 +100,30 @@ describe('CoursePage', () => {
     expect(component.pageIndex()).toBe(1);
     expect(mockCoursesService.findLessons).toHaveBeenLastCalledWith(1, '', 'asc', 1, 3);
 
+    expect(getRowDescription(de, 1)).toBe("Lesson 4");
+    expect(getRowDescription(de, 2)).toBe("Lesson 5");
+    expect(getRowDescription(de, 3)).toBe("Lesson 6");
+  });
+
+  it('should navigate to previous page', async () => {
+    component.pageIndex.set(1);
     component.prevPage();
     await fixture.whenStable();
     fixture.detectChanges();
 
     expect(component.pageIndex()).toBe(0);
     expect(mockCoursesService.findLessons).toHaveBeenLastCalledWith(1, '', 'asc', 0, 3);
+
+    expect(getRowDescription(de, 1)).toBe("Lesson 1");
+    expect(getRowDescription(de, 2)).toBe("Lesson 2");
+    expect(getRowDescription(de, 3)).toBe("Lesson 3");
   });
 
   it('should toggle sort direction', async () => {
+    await fixture.whenStable();
     fixture.detectChanges();
     expect(component.sortDirection()).toBe('asc');
+
     let sortBtn = de.query(By.css('.sort-btn'));
     expect(sortBtn.nativeElement.textContent).toContain('↑');
 
@@ -86,16 +134,16 @@ describe('CoursePage', () => {
     await fixture.whenStable();
 
     expect(component.sortDirection()).toBe('desc');
-    expect(component.pageIndex()).toBe(0);
-
     sortBtn = de.query(By.css('.sort-btn'));
     expect(sortBtn.nativeElement.textContent).toContain('↓');
+
+    const lesson = de.query(By.css("tr:first-child .description-cell"));
+
+    expect(lesson.nativeElement.textContent).toBe("Lesson 20");
   });
 
-  it('should update page size', async () => {
-    component.pageIndex.set(1);
-    component.pageSize.set(3);
 
+  it('should update page size', async () => {
     fixture.detectChanges();
     await fixture.whenStable();
 
@@ -109,12 +157,10 @@ describe('CoursePage', () => {
 
     expect(component.pageSize()).toBe(10);
     expect(component.pageIndex()).toBe(0);
+
+    expect(getRowDescription(de, 1)).toBe("Lesson 1");
+    expect(getRowDescription(de, 10)).toBe("Lesson 10");
   });
-
-  /*
-
-
-
 
   it('should debounce search input (400ms)', async () => {
     await fixture.whenStable();
@@ -127,7 +173,5 @@ describe('CoursePage', () => {
 
     expect(mockCoursesService.findLessons).toHaveBeenLastCalledWith(1, 'advanced', 'asc', 0, 3);
   });
-*/
-
 
 });
