4199929a6ef03f35e99a5ad924ff6108de6a29c5
vasco <jhades.dev@gmail.com>
2026-03-24 13:33:39 +0100

    recording Angular Testing In Depth (Signals Edition)

--- src/app/services/courses.spec.ts
+++ src/app/services/courses.spec.ts
@@ -6,6 +6,7 @@ import {Course} from '../model/course';
 import {createCourse, MOCK_COURSES} from '../testing/testing-data';
 import {TestBed} from '@angular/core/testing';
 import {provideHttpClient} from '@angular/common/http';
+import {request} from 'express';
 
 describe('CoursesService', () => {
   let service: CoursesService;
@@ -54,6 +55,32 @@ describe('CoursesService', () => {
     expect(course.id).toBe(1);
     expect(course.titles.description).toBe('Beginner Course');
   });
+
+
+  it('should find lessons by query', async () => {
+
+    const promise = service.findLessons(12, 'filter-text', 'desc', 2, 10);
+
+    const req = httpTestingController.expectOne(req => req.url === '/api/lessons');
+
+    const params = req.request.params;
+
+    expect(params.get('courseId')).toBe('12');
+    expect(params.get('filter')).toBe('filter-text');
+    expect(params.get('sortOrder')).toBe('desc');
+    expect(params.get('pageNumber')).toBe('2');
+    expect(params.get('pageSize')).toBe('10');
+
+    const mockLessons = {payload: [{id:12, description: "Lesson 1"}]};
+
+    req.flush(mockLessons);
+
+    const result = await promise;
+
+    expect(result).toBe(mockLessons.payload);
+
+
+  })
 
 
 });
