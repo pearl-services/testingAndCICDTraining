3c4ee7d80959003dd40c2bb36a375b92d0f909c6
vasco <jhades.dev@gmail.com>
2026-04-01 16:39:33 +0200

    recording Angular Testing In Depth (Signals Edition)

--- src/app/services/course-resolver.spec.ts
+++ src/app/services/course-resolver.spec.ts
@@ -5,7 +5,7 @@ import {CoursePage} from '../course-page/course-page';
 import {provideRouter, Router} from '@angular/router';
 import {courseResolver} from './course.resolver';
 import {RouterTestingHarness} from '@angular/router/testing';
-import {MOCK_COURSES} from '../testing/testing-data';
+import {MOCK_COURSES, MOCK_LESSONS} from '../testing/testing-data';
 
 describe('CourseResolver', () => {
   let mockCoursesService:any;
@@ -31,6 +31,23 @@ describe('CourseResolver', () => {
     }).compileComponents();
 
     harness = await RouterTestingHarness.create();
+  })
+
+  it("should load correct course by Id", async () => {
+
+    mockCoursesService.findCourseById.mockResolvedValueOnce(MOCK_COURSES[0]);
+
+    const component = await harness.navigateByUrl('/courses/1', CoursePage);
+
+    expect(TestBed.inject(Router).url).toBe("/courses/1");
+
+    expect(mockCoursesService.findCourseById).toHaveBeenCalledOnce();
+    expect(mockCoursesService.findCourseById).toHaveBeenLastCalledWith("1");
+
+    expect(component.course()).toEqual(MOCK_COURSES[0]);
+
+    expect(harness.routeNativeElement?.textContent).toContain("Beginner Course");
+
   })
 
 })
