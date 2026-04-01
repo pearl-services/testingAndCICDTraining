8684804355c965c1a1bfddbf98208fa1bb6a4a5e
vasco <jhades.dev@gmail.com>
2026-04-01 16:15:44 +0200

    recording Angular Testing In Depth (Signals Edition)

--- src/app/services/course-resolver.spec.ts
+++ src/app/services/course-resolver.spec.ts
@@ -8,9 +8,29 @@ import {RouterTestingHarness} from '@angular/router/testing';
 import {MOCK_COURSES} from '../testing/testing-data';
 
 describe('CourseResolver', () => {
+  let mockCoursesService:any;
+  let harness: RouterTestingHarness;
 
   beforeEach(async () => {
+    mockCoursesService = {
+      findCourseById: vi.fn()
+    }
 
+    await TestBed.configureTestingModule({
+      imports: [CoursePage],
+      providers: [
+        {provide: CoursesService, useValue: mockCoursesService},
+        provideRouter([{
+          path: 'courses/:id',
+          component: CoursePage,
+          resolve: {
+            course: courseResolver
+          }
+        }])
+      ]
+    }).compileComponents();
+
+    harness = await RouterTestingHarness.create();
   })
 
 })
